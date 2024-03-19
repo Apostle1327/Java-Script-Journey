ded40717bcf7017e1d3ba7d850623b653ca8b21c
Apostle1327 <apostle1327@gmail.com>
2024-03-19 11:21:47 +0530

    Snake & Ladders - Day-2, Added player turn optimizations

--- Snakes & Ladders/Assets/JavaScript/main.js
+++ Snakes & Ladders/Assets/JavaScript/main.js
@@ -1,39 +1,78 @@
+localStorage.setItem("player", 1);
+localStorage.setItem("player1_count", 0);
+localStorage.setItem("player2_count", 0);
+
 function rollDice() {
   let p = parseInt(localStorage.getItem("player"));
-  let ply = "";
   let chance = Math.ceil(Math.random() * 6);
 
   if (chance == 0) {
-    chance++;
+    chance = 1;
   }
 
   if (p % 2 == 0) {
     ply = 2;
-    x = parseInt(localStorage.getItem("player2_count"));
+
+    let x = parseInt(localStorage.getItem("player2_count"));
+    let y = parseInt(localStorage.getItem("player1_count"));
 
     if (x != 0) {
-      b2 = document.getElementById("b" + x);
-      b2.style.backgroundColor = "";
+      // this condition is to check if both the Players are at the same spot.
+      if (x == y) {
+        let b1 = document.getElementById("b" + x);
+        b1.style.backgroundColor = "yellow";
+      } else {
+        let b1 = document.getElementById("b" + x);
+        b1.style.backgroundColor = "";
+      }
     }
 
     x += chance;
     localStorage.setItem("player2_count", x);
 
-    b2 = document.getElementById("b" + x);
-    b2.style.backgroundColor = "green";
+    if (x == y) {
+      let b1 = document.getElementById("b" + x);
+      b1.style.backgroundColor = "cyan";
+    } else {
+      let b1 = document.getElementById("b" + x);
+      b1.style.backgroundColor = "green";
+      b1.style.transition=""
+    }
+
+    if (chance == 6) {
+      p--;
+    }
   } else {
     ply = 1;
-    x = parseInt(localStorage.getItem("player1_count"));
+
+    let x = parseInt(localStorage.getItem("player1_count"));
+    let y = parseInt(localStorage.getItem("player2_count"));
 
     if (x != 0) {
-      b1 = document.getElementById("b" + x);
-      b1.style.backgroundColor = "";
+      // this condition is to check if both the Players are at the same spot.
+      if (x == y) {
+        let b1 = document.getElementById("b" + x);
+        b1.style.backgroundColor = "green";
+      } else {
+        let b1 = document.getElementById("b" + x);
+        b1.style.backgroundColor = "";
+      }
     }
+
     x += chance;
     localStorage.setItem("player1_count", x);
 
-    b1 = document.getElementById("b" + x);
-    b1.style.backgroundColor = "yellow";
+    if (x == y) {
+      let b1 = document.getElementById("b" + x);
+      b1.style.backgroundColor = "cyan";
+    } else {
+      let b1 = document.getElementById("b" + x);
+      b1.style.backgroundColor = "yellow";
+    }
+
+    if (chance == 6) {
+      p--;
+    }
   }
 
   document.getElementById("d1").innerHTML = "Player-" + ply + ":" + chance;
